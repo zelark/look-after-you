0dd3be42ccd8c209c28d9ee517e568f99f7cf832
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-07-08 14:06:04 +0300

    Add play button

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ function get_random_int(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-window.onload = function () {
-  const n      = (get_random_int(COUNT) + 1).toString();
-  const track  = "tracks/" + n.padStart(2, "0") + ".mp3";
-  const player = document.getElementById("player");
+const n      = (get_random_int(COUNT) + 1).toString();
+const track  = "tracks/" + n.padStart(2, "0") + ".mp3";
+const player = document.getElementById("player");
 
-  player.innerHTML = '<source src="' + track + '" type="audio/mpeg"></source>';
-  window.setTimeout(function() {
-    console.log(track);
-    player.volume = 0.7;
+player.innerHTML = '<source src="' + track + '" type="audio/mpeg"></source>';
+
+function play() {
+  if (player.paused) {
     player.play();
-  }, 1000);
+  } else {
+    player.pause();
+  }
+}
+
+window.onload = function () {
 }
