1f3d73fad1f9538ce0bb4d98d55f50643b636ec2
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2019-04-27 15:22:32 +0300

    Add play and stop buttons

--- script.js
+++ script.js
@@ -2,28 +2,40 @@ const COUNT = 4; // number of tracks
 
 
 const getRandomInt = max =>
-  Math.floor(Math.random() * Math.floor(max));
+  Math.floor(Math.random() * Math.floor(max)) + 1;
 
 
 const trackName = n => `tracks/${n}.mp3`;
 
 
 const howl = track =>
-  new Howl({ src: [track], autoplay: true, volume: 0.8 });
+  new Howl({ src: [track], autoplay: false, volume: 0.8 });
 
 
-const next = (rand) => {
-  let n = rand();
-  let player = howl(trackName(n));
+let n = getRandomInt(COUNT);
+let player = howl(trackName(n));
 
-  return () => {
-    player.stop();
-    n = rand();
-    player = howl(trackName(n));
-    console.log(player._src);
-  };
+const play = () => {
+  if (player._sounds.length > 0) {
+    player.play(player._sounds[0]._id);
+  } else {
+    player.play();
+  }
+}
+
+const next = () => {
+  player.stop();
+  n = getRandomInt(COUNT);
+  player = howl(trackName(n));
+  play();
+  console.log(player._src);
 };
 
 
-const button = document.getElementById('button');
-button.addEventListener('click', next(() => getRandomInt(COUNT) + 1));
+const stopTrack = document.getElementById('stop-btn');
+const playTrack = document.getElementById('play-btn');
+const nextTrack = document.getElementById('next-btn');
+
+stopTrack.addEventListener('click', () => player.stop());
+playTrack.addEventListener('click', () => play());
+nextTrack.addEventListener('click', () => next());
